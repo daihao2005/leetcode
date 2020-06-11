e5d8bd15b49760ace5ea94a9d07acdbf9136f5d6
wanggang <wg@yxqiche.com>
2020-06-11 11:24:18 +0800

    P6

--- src/com/daihao/P6.java
+++ src/com/daihao/P6.java
@@ -3,8 +3,8 @@ package com.daihao;
 public class P6 {
     public static void main(String[] args) {
 
-        String s = "PAYPALISHIRING";
-        int numRows = 4;
+        String s = "A";
+        int numRows = 1;
         String convert = convert(s, numRows);
         System.out.println(convert);
 
@@ -12,28 +12,38 @@ public class P6 {
 
     public static String convert(String s, int numRows) {
 
+        if (s == null || "".equals(s)) {
+            return s;
+        }
+        if (numRows <= 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numRows; i++) {
             int j = 0;
             while (true) {
                 if (j % (numRows - 1) == 0) {
                     int index = (2 * numRows - 2) * j / (numRows - 1) + i;
-                    if(index>s.length()-1){
+                    if (index > s.length() - 1) {
                         break;
                     }
+                    sb.append(s.charAt(index));
+                    System.out.print(s.charAt(index));
+                } else if ((i + j) % (numRows - 1) == 0) {
+                    int index = ((j / (numRows - 1)) + 1) * (2 * numRows - 2) - i;
+                    if (index > s.length() - 1) {
+                        break;
+                    }
+                    sb.append(s.charAt(index));
                     System.out.print(s.charAt(index));
                 } else {
-                  if(  (i+j)%(numRows-1)==0){
-                      System.out.print("#");
-                  }else{
-                      System.out.print(" ");
-                  }
+                    System.out.print(" ");
                 }
-
                 j++;
             }
 
             System.out.println();
         }
-        return null;
+        return sb.toString();
     }
 }
