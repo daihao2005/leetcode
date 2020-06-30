115fb22500d9c2cdbcbf3944baf6eaf987b45a12
王刚 <wg@yxqiche.com>
2020-06-30 10:39:03 +0800

    P101

---
+++ src/com/daihao/P100.java
@@ -0,0 +1,22 @@
+package com.daihao;
+
+public class P100 {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        if (!isSameTree(p.left, q.left)) {
+            return false;
+        }
+        if (!isSameTree(p.right, q.right)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/com/daihao/P101.java
@@ -0,0 +1,25 @@
+package com.daihao;
+
+import java.util.ArrayDeque;
+
+public class P101 {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        return isSymmetric(root, root);
+    }
+
+    public boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+
+    }
+}
